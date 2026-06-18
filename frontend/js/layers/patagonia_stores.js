fa271d582696a8817f86b424cffe6c0d1409aa77
Joel Karie <joelkarie@gmail.com>
2026-06-18 10:38:13 -0400

    Add custom marker to patagonia map.

--- frontend/js/layers/patagonia_stores.js
+++ frontend/js/layers/patagonia_stores.js
@@ -2,11 +2,20 @@ export function createPatagoniaStoreLayer(stores) {
     const storeLayer = L.layerGroup();
 
     stores.forEach(store => {
-
+        const customMarker = L.divIcon({
+            html: `
+                <img src="assets/patagonia_fish.png"
+                    style="
+                        width:76.8px;
+                        filter: drop-shadow(2px 4px 3px rgba(0,0,0,0.4));
+                    ">
+            `,
+            className: "",
+            iconAnchor: [38,43]
+        });
         L.marker([
             store.latitude,
-            store.longitude
-        ])
+            store.longitude], { icon: customMarker })
             .addTo(storeLayer)
             .bindPopup(`
                 <b>${store.store_name}</b><br>
