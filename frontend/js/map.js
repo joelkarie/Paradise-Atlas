c247c1ce0b4ef88d3dbffd3dd63178526997e773
Joel Karie <joelkarie@gmail.com>
2026-06-26 00:35:46 -0400

    Prevent Leaflet doom scroll.

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -3,8 +3,13 @@ export function createMap() {
     const isMobile = window.innerWidth < 768;
 
     const map = L.map('map', {
+        maxBounds: [
+            [-85, -180],
+            [85, 180]
+        ],
+        maxBoundsViscosity: 1.0,
         zoomControl: !isMobile
-    }); 
+    });
     map.setView(
         [39.5, -98.35],
         4
