6d21901b3e8c3ad4c6b4e2f9b82af58689f6e3e5
Joel Karie <joelkarie@gmail.com>
2026-06-28 00:35:19 -0400

    Change color and fine tune visit page.

--- frontend/js/layers/visit_order.js
+++ frontend/js/layers/visit_order.js
@@ -7,12 +7,12 @@ export function createVisitOrderLayer(locations) {
             className: "numbered-marker",
             html: stopNum,
             iconSize: [35, 35],
-            iconAnchor: [17, 35]  
+            iconAnchor: [17, 35]
 
         });
         L.marker([
             location.latitude,
-            location.longitude], {icon: customMarker})
+            location.longitude], { icon: customMarker })
             .addTo(visitOrderLayer)
             .bindPopup(`
                 <b>${location.location_name}</b><br>
@@ -20,7 +20,7 @@ export function createVisitOrderLayer(locations) {
                 ${location.date ? `<br><i>Visited on ${location.date}</i>` : ''}
             `);
         stopNum += 1
-    
+
     });
 
     const route = locations.map(location => [
@@ -29,44 +29,9 @@ export function createVisitOrderLayer(locations) {
     ]);
 
     L.polyline(route, {
-        color: "blue",
-        weight: 4
+        color: #a797a9,
+        weight: 2
     }).addTo(visitOrderLayer);
 
     return visitOrderLayer;
 }
-    // # Number the cities in order of visit.
-    // stop_num = 1
-
-    // # Empty map
-    // m = folium.Map(
-    //     location=LOWER48_FOCUS_MAP_LOCATION[0],
-    //     tiles="CartoDB Voyager",
-    //     zoom_start=LOWER48_FOCUS_MAP_LOCATION[1],
-    // )
-
-    // script_monitor("Adding points to lines map for:", speak=False)
-    // # Iterate through dataframe and add points to map
-    // for city in range(0, len(df)):
-    //     script_monitor(df.iloc[city]["City"], speak=False)
-    //     icon_number = folium.plugins.BeautifyIcon(
-    //         # border_color="#00ABCD",
-    //         border_color="#b0c996",
-    //         border_width=2,
-    //         text_color="#000000",
-    //         background_color="#fefdfd",
-    //         number=stop_num,
-    //         inner_icon_style="""
-    //             display:flex;
-    //             justify-content:center;
-    //             align-items:center;
-    //             font-size:12px;
-    //             margin-top:1px;
-    //         """,
-    //     )
-    //     folium.Marker(
-    //         location=[df.iloc[city]["Latitude"], df.iloc[city]["Longitude"]],
-    //         popup=df.iloc[city]["City"],
-    //         icon=icon_number,
-    //     ).add_to(m)
-    //     stop_num += 1
