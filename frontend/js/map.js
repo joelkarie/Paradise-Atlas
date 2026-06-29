a16ce623d5499174767388f5761653bf4a61fa77
Joel Karie <joelkarie@gmail.com>
2026-06-29 19:23:38 -0400

    Cleanup 5.

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -14,6 +14,7 @@ export function createMap() {
         [39.5, -98.35],
         4
     );
+    
     // Standard OpenStreetMap
     // L.tileLayer(
     //     'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
