b098119b57726dac0ce00398866c42d95cdd00e3
Joel Karie <joelkarie@gmail.com>
2026-06-29 16:26:49 -0400

    CHange to watercolor tileset.

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -14,12 +14,16 @@ export function createMap() {
         [39.5, -98.35],
         4
     );
-    L.tileLayer(
-        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        {
-            attribution: '&copy; OpenStreetMap contributors'
-        }
-    ).addTo(map);
+    // L.tileLayer(
+    //     'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    //     {
+    //         attribution: '&copy; OpenStreetMap contributors'
+    //     }
+    // ).addTo(map);
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg', {
+        attribution: '&copy; Stadia Maps &copy; Stamen Design &copy; OpenStreetMap contributors',
+        maxZoom: 16
+    }).addTo(map);
     map.touchZoom.enable();
     map.doubleClickZoom.enable();
     map.scrollWheelZoom.disable();
