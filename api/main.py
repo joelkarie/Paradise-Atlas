ff13cf9e3747294b20939a2c8e17844ed9f3e490
Joel Karie <joelkarie@gmail.com>
2026-07-06 00:16:24 -0400

    Remove commented lines.

--- api/main.py
+++ api/main.py
@@ -1,16 +1,13 @@
 import os
-# from pwdlib import PasswordHash
 from pathlib import Path
 from dotenv import load_dotenv
-# from itsdangerous import URLSafeSerializer
 
 load_dotenv()
 
-from fastapi import FastAPI, Form, Response, Request
+from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.templating import Jinja2Templates
 
 from api.auth import password_hash, serializer
 from api.database import engine
@@ -26,21 +23,13 @@ from api.routers.michael_could_live_router import router as michael_could_live_r
 from api.routers.together_could_live_router import router as together_could_live_router
 from api.services.locations_services import (
     get_location_ratings,
-    get_location_types,
-    get_locations_for_dropdown,
-    create_location,
-    create_location_rating,
 )
 from api.routers.admin_router import router as admin_router
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 app = FastAPI()
 
-# password_hash = PasswordHash.recommended()
-
-# serializer = URLSafeSerializer(os.getenv("SECRET_KEY"), salt="session")
-
-# templates = Jinja2Templates(directory="frontend/templates")
 
 app.add_middleware(
     CORSMiddleware,
@@ -149,121 +138,3 @@ def logout():
     response = RedirectResponse(url="/login")
     response.delete_cookie("session")
     return response
-
-
-# def get_current_user(request: Request):
-#     token = request.cookies.get("session")
-
-#     if not token:
-#         return None
-
-#     try:
-#         data = serializer.loads(token)
-#         return data.get("user")
-#     except Exception:
-#         return None
-
-
-# def require_admin(request: Request):
-#     token = request.cookies.get("session")
-
-#     if not token:
-#         raise Exception("NOT_AUTHORIZED")
-
-#     try:
-#         data = serializer.loads(token)
-#         return data.get("user")
-#     except Exception:
-#         raise Exception("NOT_AUTHORIZED")
-
-
-# @app.get("/admin/home")
-# def admin_home_page(request: Request):
-#     try:
-#         user = require_admin(request)
-#     except Exception:
-#         return RedirectResponse("/login")
-
-#     return FileResponse(BASE_DIR / "frontend" / "admin_home.html")
-
-
-# @app.get("/admin/joel")
-# def joel_admin_page(request: Request):
-#     try:
-#         user = require_admin(request)
-#     except Exception:
-#         return RedirectResponse("/login")
-
-#     return FileResponse(BASE_DIR / "frontend" / "joel_admin.html")
-
-
-# @app.get("/admin/michael")
-# def michael_admin_page(request: Request):
-#     try:
-#         user = require_admin(request)
-#     except Exception:
-#         return RedirectResponse("/login")
-
-#     return FileResponse(BASE_DIR / "frontend" / "michael_admin.html")
-
-
-# @app.get("/admin/add_visit")
-# def add_visit_page(request: Request):
-
-#     location_types = get_location_types()
-#     locations = get_locations_for_dropdown()
-
-#     return templates.TemplateResponse(
-#         request=request,
-#         name="add_visit.html",
-#         context={
-#             "location_types": location_types,
-#             "locations": locations,
-#         },
-#     )
-
-
-# @app.post("/admin/add_visit")
-# def add_visit(
-#     location_type: int = Form(...),
-#     location_id: str = Form(...),
-#     visit_date: str = Form(...),
-#     new_location_name: str = Form(""),
-#     new_state: str = Form(""),
-#     new_country: str = Form("USA"),
-# ):
-#     print(f"Location Type: {location_type}")
-#     print(f"Location ID: {location_id}")
-#     print(f"Visit Date: {visit_date}")
-
-#     print(f"New Name: {new_location_name}")
-#     print(f"State: {new_state}")
-#     print(f"Country: {new_country}")
-
-#     next_visit_order = None
-#     if location_id == "new":
-#         location_id = create_location(
-#             name=new_location_name,
-#             location_type_id=location_type,
-#             state_province=new_state,
-#             country=new_country,
-#         )
-
-#         print(f"New location created with id = {location_id}")
-#         next_visit_order =get_next_visit_order()
-#         print(f"Next visit order = {next_visit_order}")
-#         create_location_rating(location_id)
-    
-#     next_visit_number = get_next_visit_number()
-#     print(f"Next visit number = {next_visit_number}")
-
-#     visit_id = create_visit(
-#         location_id=location_id,
-#         visit_date=visit_date,
-#         visit_number=next_visit_number,
-#         visit_order=next_visit_order,
-#     )
-
-#     print(f"Visit created with id = {visit_id}")
-
-#     return {"success": True}
