d0932948b6b1f4a323c196d3c085b69670a505a0
Joel Karie <joelkarie@gmail.com>
2026-07-09 11:25:55 -0400

    Debug theatre image post api 8.

--- api/services/theatre_services.py
+++ api/services/theatre_services.py
@@ -34,7 +34,7 @@ def get_theatre(theatre_id: int):
         row = conn.execute(
             text("""
                 SELECT *
-                FROM theatre_view
+                FROM theatre
                 WHERE id = :id
             """),
             {"id": theatre_id},
