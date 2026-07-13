f559acd47f80339c01a73de9be79da9c53d0d166
Joel Karie <joelkarie@gmail.com>
2026-07-13 00:18:12 -0400

    Add capitols/legs to gazetteer.

--- api/routers/gazetteer_router.py
+++ api/routers/gazetteer_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from api.services.locations_services import get_locations
+from ..services.capitols_services import get_capitols
 
 router = APIRouter(prefix="/gazetteer", tags=["Gazetteer"])
 
@@ -10,13 +11,16 @@ templates = Jinja2Templates(directory="frontend/templates/gazetteer")
 @router.get("/home")
 def gazetteer_home_page(request: Request):
 
-    # locations = get_locations()
     locations_sorted = sorted(get_locations(), key=lambda x: x["name"].lower())
 
     city_count = sum(1 for location in locations_sorted if location["location_type_id"] == 1)
     parks_count = sum(1 for location in locations_sorted if location["location_type_id"] == 2)
     landmarks_count = sum(1 for location in locations_sorted if location["location_type_id"] == 3)
     
+    us_capitols_sorted = sorted(get_capitols(), key=lambda x: x["state_province"].lower())
+    us_capitol_count = len(us_capitols_sorted)
+
+
     return templates.TemplateResponse(
         request=request,
         name="gazetteer_home.html",
@@ -24,6 +28,8 @@ def gazetteer_home_page(request: Request):
             "locations": locations_sorted,
             "city_count": city_count,
             "parks_count": parks_count,
-            "landmarks_count": landmarks_count
+            "landmarks_count": landmarks_count,
+            "us_capitols": us_capitols_sorted,
+            "us_capitol_count": us_capitol_count
         },
     )
