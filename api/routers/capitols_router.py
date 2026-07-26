ea5f52287e042d0cb655d26f50c18e1ecf9e1e1f
Joel Karie <joelkarie@gmail.com>
2026-07-25 20:04:22 -0400

    Add api to get capitols for app.

--- api/routers/capitols_router.py
+++ api/routers/capitols_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.services.capitols_services import get_capitols
+from api.services.capitols_services import get_capitols, get_capitols_for_app
 
 router = APIRouter(prefix="/capitols", tags=["Capitols Visited"])
 
@@ -7,3 +7,8 @@ router = APIRouter(prefix="/capitols", tags=["Capitols Visited"])
 @router.get("")
 def capitols():
     return get_capitols()
+
+@router.get("/for_app")
+def capitols_for_app():
+    return get_capitols_for_app()
+
--- api/services/capitols_services.py
+++ api/services/capitols_services.py
@@ -12,3 +12,26 @@ def get_capitols():
         """))
 
         return [dict(row._mapping) for row in rows]
+
+def get_capitols_for_app():
+
+    with engine.connect() as conn:
+
+        rows = conn.execute(text("""
+            SELECT 
+            cap.id as id, 
+            l.name as city, 
+            l.state_province as state_province, 
+            v."date" as date,
+            l.latitude AS latitude, 
+            l.longitude AS longitude, 
+            cap.fact AS fact, cap.architect AS architect, 
+            cap.architectural_style AS architectural_style, 
+            cap.year_completed AS year_completed
+            FROM visit v
+            JOIN location l on l.id = v.location_id
+            JOIN capitol cap ON cap.id = v.capitol_id
+            ORDER BY l.state_province ASC
+        """))
+
+        return [dict(row._mapping) for row in rows]
